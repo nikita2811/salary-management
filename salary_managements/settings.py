008a61525431e76702483532ce666c0a6539a3bc
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2026-08-31 11:37:34 +0530

    code update : render url added to allowed hosts

--- salary_managements/settings.py
+++ salary_managements/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-04^5561t-!yid*+bu*5hy+9m=lfydha^u9p*1wo3c2-5fz)(uj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','salary-badt.onrender.com']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 

38f4503fe50bdb0e33d9817426c8b5cbc166b602
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2026-08-31 08:50:07 +0530

    feat :  CRUD apis created to pass test cases

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Employee
+from .serializers import EmployeeSerializer, EmployeeListSerializer
+
+
+# -- used modelviewset for CRUD operations
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all().order_by('id')
+    serializer_class = EmployeeSerializer
+    
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return EmployeeListSerializer   # lightweight for table view
+        return EmployeeSerializer
+
+    def get_queryset(self):
+        return Employee.objects.filter(is_active=True).order_by('id')
+    
+    # -- delete api for soft delete
+    def destroy(self, request, *args, **kwargs):
+        instance           = self.get_object()
+        instance.is_active = False
+        instance.save()
+        return Response({"message": "Employee deleted."}, status=status.HTTP_204_NO_CONTENT)
+    
+    
--- salary_managements/settings.py
+++ salary_managements/settings.py
@@ -82,6 +82,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'salary_managements.wsgi.application'
 
+REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
+
 
 LOGGING = {
     "version": 1,
